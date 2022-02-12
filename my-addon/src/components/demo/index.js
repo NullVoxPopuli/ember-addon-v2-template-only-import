18e17c94e96b603e384c72d8fdb229f0d67a5fa8
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-02-12 17:27:38 -0500

    Things look to almost be working with: https://github.com/embroider-build/embroider/pull/1126

--- my-addon/src/components/demo/index.js
+++ my-addon/src/components/demo/index.js
@@ -2,9 +2,11 @@ import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 
 import FlipButton from './button';
+import BlahButton from './button.hbs';
 
 export default class Demo extends Component {
   Button = FlipButton;
+  Button2 = BlahButton;
 
   @tracked active = false;
 
--- testing/ember-app/tests/_the_-test.js
+++ testing/ember-app/tests/_the_-test.js
@@ -1,29 +1,10 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { clearRender, click, render } from '@ember/test-helpers';
+import { click, render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 
-import { getService } from 'my-addon';
-
 module('v2 addon tests', function (hooks) {
   setupRenderingTest(hooks);
-
-  test('utils work', function (assert) {
-    assert.ok(getService(this, 'router'));
-  });
-
-  test('{{stringify obj}}', async function (assert) {
-    this.setProperties({
-      testObj: { a: 'hi', b: 'hello' },
-    })
-
-    await render(hbs`<pre>{{stringify this.testObj}}</pre>`);
-
-    assert.dom('pre').containsText('hi');
-    assert.dom('pre').containsText('hello');
-  });
 
   test('<Demo />', async function (assert) {
     await render(hbs`<Demo />`);
